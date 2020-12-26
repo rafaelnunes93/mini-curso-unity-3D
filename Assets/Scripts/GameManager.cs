3a91b49e51ee40d0ebbd8d8c149868ffa61ddd73
Rafael Nunes <rafaelnunes_93@hotmail.com>
2020-12-26 18:40:44 -0300

    Rain particle

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 
 public enum enemyState
@@ -19,16 +20,84 @@ public class GameManager : MonoBehaviour
     public float slimeAlertTime = 3f;
     public float slimeAtackDelay = 1f;
 
+    [Header("Rain manager")]
+    public PostProcessVolume postB;
+    public ParticleSystem rainParticle;
+    private ParticleSystem.EmissionModule rainModule;
+    public int rainRateOverTime;
+    public int rainIncrement;
+    public float rainIncrementDelay;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        rainModule = rainParticle.emission;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void onOfRain(bool isRain)
     {
-        
+        StopCoroutine("RainManager");
+        StopCoroutine("PostBManager");
+        StartCoroutine("RainManager", isRain);
+        StartCoroutine("PostBManager", isRain);
+    }
+
+    IEnumerator RainManager(bool isRain)
+    {
+        switch (isRain)
+        {
+            case true:
+
+                for(float r = rainModule.rateOverTime.constant; r < rainRateOverTime; r += rainIncrement)
+                {
+                    rainModule.rateOverTime = r;
+                    yield return new WaitForSeconds(rainIncrementDelay);
+                }
+
+                rainModule.rateOverTime = rainRateOverTime;
+                    
+                break;
+
+            case false:
+                for (float r = rainModule.rateOverTime.constant; r > 0 ; r -= rainIncrement)
+                {
+                    rainModule.rateOverTime = r;
+                    yield return new WaitForSeconds(rainIncrementDelay);
+                }
+
+                rainModule.rateOverTime = 0;
+
+                break;
+        }
+    }
+
+    IEnumerator PostBManager(bool isRain)
+    {
+        switch (isRain)
+        {
+            case true:
+
+                for (float w = postB.weight; w < 1; w += 1 * Time.deltaTime)
+                {
+                    postB.weight = w;
+                    yield return new WaitForEndOfFrame();
+                }
+
+                postB.weight = 1;
+
+                break;
+
+            case false:
+
+                for (float w = postB.weight; w > 0; w -= 1 * Time.deltaTime)
+                {
+                    postB.weight = w;
+                    yield return new WaitForEndOfFrame();
+                }
+
+                postB.weight = 0;
+
+                break;
+        }
     }
 }
---
+++ Assets/Scripts/RainManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainManager : MonoBehaviour
+{
+    private GameManager _gameManager;
+    public bool isRain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameManager = FindObjectOfType(typeof(GameManager)) as GameManager;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            _gameManager.onOfRain(isRain);
+        }
+    }
+}
